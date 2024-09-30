d4a60d53df781a2307da49fee773a9ff9c330dbd
maytham1988 <maytham.saeidi@gmail.com>
2024-10-01 01:03:18 +0330

    Automatic commit

--- getproxy.py
+++ getproxy.py
@@ -4,11 +4,12 @@ import requests
 from git import Repo
 import time
 
-repo_path = '/root/grasspr/'  # Path to the Git repository
+# Path to the Git repository
+repo_path = '/data/data/com.termux/files/home/getgrass'
 
 while True:
     # Download the file from the URL
-    url = 'https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/all.txt'
+    url = 'https://raw.githubusercontent.com/monosans/p'  # تکمیل آدرس URL در اینجا ضروری است
     response = requests.get(url)
     with open('all.txt', 'wb') as f:
         f.write(response.content)
@@ -22,11 +23,6 @@ while True:
 
     # Commit changes
     repo.index.commit('Automatic commit')
-
-    # Push changes to the 'main' branch of the remote repository
-    origin = repo.remote(name='origin')
-    origin.push('main')
-
-    # Wait for 5 minutes before running the loop again
-    time.sleep(5 * 60)
-
+    
+    # افزودن یک تایمر به منظور جلوگیری از اجرای مکرر بدون وقفه
+    time.sleep(60)  # اجرای حلقه هر 60 ثانیه
